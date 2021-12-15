6d541cf195b099a0822453ce0eb5b0dd4bcd8a10
Francis Grizzly Smit <grizzly@smit.id.au>
2021-12-15 22:30:21 +1100

    fmtlib 1.0.31-0

--- build/testcmplx.h
+++
@@ -1,4 +0,0 @@
-// the configured options and settings for testcmplx
-#define testcmplx_VERSION_MAJOR 1
-#define testcmplx_VERSION_MINOR 0
-#define testcmplx_VERSION_PATCH 30
--- testcmplx.h
+++ testcmplx.h
@@ -1,1 +1,4 @@
-build/testcmplx.h
+// the configured options and settings for testcmplx
+#define testcmplx_VERSION_MAJOR 1
+#define testcmplx_VERSION_MINOR 0
+#define testcmplx_VERSION_PATCH 30
